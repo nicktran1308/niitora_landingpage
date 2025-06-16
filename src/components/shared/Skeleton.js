26b81e67e387742885b0637ac71ed01cd44ca4a1
Nick Tran <123424016+nicktran1308@users.noreply.github.com>
2025-06-15 23:58:46 -0700

    Updates

--- src/components/shared/Skeleton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../../styles/shared/Skeleton.css';
-
-const Skeleton = ({ type }) => {
-  const classes = `skeleton ${type}`;
-  return <div className={classes}></div>;
-};
-
-export default Skeleton; 
